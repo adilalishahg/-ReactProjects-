87fde9ad3024e4292450dfb3a2ab1e8a87cfe00f
adilalishahg <adilalishahg@gmail.com>
2024-06-26 12:27:31 -0700

    [ReactProjects]

---
+++ calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    <div id="container">
+       <input type="text" id="display"/>
+       <div id="button-container">
+          <button>C</button>
+       </div>
+    </div>
+  )
+}
+
+export default App
